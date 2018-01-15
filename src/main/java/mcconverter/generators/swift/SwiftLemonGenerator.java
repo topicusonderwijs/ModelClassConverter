58e57f756a4c36fff4a6438aaab721a7e069efa9
Thomas Dekker <thomas.dekker@topicus.nl>
2018-01-15 14:38:26 +0100

    Renamed to coder

--- src/main/java/mcconverter/generators/swift/SwiftLemonGenerator.java
+++ src/main/java/mcconverter/generators/swift/SwiftLemonGenerator.java
@@ -58,7 +58,7 @@ public class SwiftLemonGenerator extends SwiftGenerator {
 			if (customTransform != null) {
 				String transform = customTransform.getTransform();
 				if (transform != null) {
-					reading += ", transform: " + transform;
+					reading += ", coder: " + transform;
 				}
 			}
 		}
