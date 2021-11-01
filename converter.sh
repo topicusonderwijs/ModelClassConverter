c609fc1a213b0eb6c934e4ff6fe2996449e3901a
Hielke Hoeve <hielke.hoeve@gmail.com>
2021-11-01 11:25:46 +0100

    revert changes for remote github actions runners, we're running them ourselves

--- converter.sh
+++ converter.sh
@@ -9,13 +9,6 @@ CONFIGURATION="configuration.xml"
 DIRECTORY="converter"
 EXECUTABLE="ModelClassConverter"
 DOWNLOAD_ARTIFACTS="download"
-GITHUB_URL="git@github.com:"
-
-# If user and token is set then use https urls
-if [ -n "$GITHUB_TOKEN" ]
-then
-	GITHUB_URL="https://$GITHUB_TOKEN@github.com/"
-fi
 
 # Determine which action to perform
 if [ -n "$1" ]
@@ -44,9 +37,6 @@ fi
 echo "converter.sh"
 echo "\tAction: $ACTION"
 echo "\tBranch: $BRANCH"
-echo "\tGithub user: $GITHUB_USER"
-echo "\tGithub URL: $GITHUB_URL"
-echo `pwd`
 
 # Run different actions
 
@@ -76,30 +66,21 @@ then
 
 	if [ ! -d "$DIRECTORY" ]
 	then
-		git clone "$GITHUB_URL"topicusonderwijs/ModelClassConverter.git "$DIRECTORY"
+		git clone git@github.com:topicusonderwijs/ModelClassConverter.git "$DIRECTORY"
 	else
 		echo "Already cloned repository"
 	fi
 
 	cd "$DIRECTORY"
 	git checkout "$BRANCH"
-	echo `pwd`
 	
 	EXECUTABLE="$EXECUTABLE-$BRANCH"
 
 	if [ ! -f "target/$EXECUTABLE.jar" ]
 	then
-		if [ -n $GITHUB_TOKEN ]
-		then
-			echo "running maven using custom settings.xml"
-			mvn clean compile assembly:single -U -s ../settings.xml
-		else
-			echo "running maven using system settings.xml"
-			mvn clean compile assembly:single -U
-		fi
-
+		mvn clean compile assembly:single -U
+		
 		cd target
-		echo `pwd`
 		mv ModelClassConverter*jar "$EXECUTABLE.jar"
 		cd ..
 		
@@ -117,20 +98,11 @@ then
 		
 		# Determine artifacts
 		DEPENDENCIES=`perl -e 'while ($_ = <>) { /<dependency name=\"(.*?)\"/; if ( $t ne $1 ) { print "$1\n"; $t = $1; } }' < $CONFIGURATION`
-		
-		echo `pwd`
 
 		# Download artifacts
 		for DEPENDENCY in $DEPENDENCIES
 		do
-			if [ -n $GITHUB_TOKEN ]
-			then
-				echo "running maven using custom settings.xml"
-				mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dartifact="$DEPENDENCY" -s settings.xml
-			else
-				echo "running maven using system settings.xml"
-				mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dartifact="$DEPENDENCY"
-			fi
+			mvn org.apache.maven.plugins:maven-dependency-plugin:get -Dartifact="$DEPENDENCY"
 		done
 		
 	fi
